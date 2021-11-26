499351203e94c4cdd14305fc49659cd29b06ed45
awadhy <awadhym@outlook.com>
2021-11-26 13:42:20 +0000

    Improved functionality - 'how's it going sit down' would register 'it' twice, and 'i' three times

--- Sub_Strings.rb
+++ Sub_Strings.rb
@@ -1,20 +1,23 @@
 require 'pry-byebug'
 
-#Code asking the user for input 
-puts "Enter your message here"
-text = gets.chomp.downcase 
-
 #dictionary array 
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+binding.pry 
 
 #Method that will create new hash
 def word_counter(text, dictionary)
   array = text.split(/[^-a-zA-Z]/)
-  frequency_of_words_in_text = Hash.new()
+  frequency_of_words_in_text = Hash.new(0)
   array.each do |word|
     next if word.empty? == true
-    next if dictionary.count(word) == 0 
-     frequency_of_words_in_text[word] = array.count(word)
+    next if dictionary.include?(word) == false
+     frequency_of_words_in_text[word] += 1
+     dictionary.each do |dict_word|
+      if word.include?(dict_word) == true && word != dict_word
+        frequency_of_words_in_text[dict_word] += 1
+      end
+  end
   end
   if frequency_of_words_in_text.empty? == true
     puts "None of the words you entered are part of this specific dictionary"
@@ -24,5 +27,9 @@ def word_counter(text, dictionary)
   end
 end
 
-word_counter(text,dictionary)
+
+word_counter("Howdy partner, sit down! How's it going?", dictionary)
+
+#word_counter("Howdy partner, sit down! How's it going?",dictionary) 
+#returns 
 
