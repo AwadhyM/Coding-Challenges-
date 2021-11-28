3110ecb85178b7c6fdf85da5c0896a5ed0e7da76
awadhy <awadhym@outlook.com>
2021-11-28 15:09:07 +0000

    Bubble sort algorithim complete

---
+++ Bubble-Sort.rb
@@ -0,0 +1,19 @@
+#can't use #sort 
+
+def bubble_sort(arr)
+  i = 0;
+  while i < arr.length - 1
+    if arr[i] < arr[i+1]
+      i+=1
+    elsif arr[i] > arr[i+1]
+    arr[i], arr[i+1] = arr[i+1], arr[i]
+    i += 1
+   end
+end
+arr
+end
+
+print bubble_sort([4,3,78,2,0,2])
+print bubble_sort([1,2,3,4,5,6])
+
+
