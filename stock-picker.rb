73480e6f9d51b15cc48de472c890ef41e2f6a13e
awadhy <awadhym@outlook.com>
2021-11-28 10:26:13 +0000

    Problem solved. Solved test case which involves duplicates

--- stock-picker.rb
+++ stock-picker.rb
@@ -12,11 +12,15 @@ def stock_picker(array)
   combos = combos.sort {|combo1, combo2| (combo1[0]-combo1[1]) <=> (combo2[0] - combo2[1])}
   most_profit = combos[0]
   new_array.push(array.index(most_profit[0]))
-  new_array.push(array.index(most_profit[1]))
+  if array.count(most_profit[1]) > 1 
+    while array.count(most_profit[1]) > 1 do 
+      array[array.index(most_profit[1])] = nil
+    end
+  end
+  new_array.push(array.index((most_profit[1])))
   return new_array
 end 
 
-print stock_picker([17,6,9,15,8,6,1,10,3])
+print stock_picker([17,3,6,9,15,8,6,1,10]) ## returns 1,4 which means buy on 2nd day $3 and sell on 4th day 15$ = 12$ profit
+print stock_picker([5,8,1,5,8]) ## returns 2,4 which means buy on 3rd day $1 and sell on 5th day 8$ = 7$ profit
 
-
-# 2,4 but your method will return 2,1
