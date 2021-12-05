b8f2eb70ab264f59b19286922f232175f7ecdf57
awadhy <awadhym@outlook.com>
2021-12-05 20:44:18 +0000

    Refactored code using Rubocop

--- Bubble-Sort.rb
+++ Bubble-Sort.rb
@@ -1,23 +1,17 @@
-require "pry-byebug"
-
-#can't use #sort 
-
 def bubble_sort(arr)
-  i = 0;
-  while i < arr.length-1
-    if arr[i] < arr[i+1]
-      i+=1
-    elsif arr[i] > arr[i+1]
-    arr[i], arr[i+1] = arr[i+1], arr[i]
-    i = 0
-    else 
-      i+=1
-   end
+  i = 0
+  while i < arr.length - 1
+    if arr[i] < arr[i + 1]
+      i += 1
+    elsif arr[i] > arr[i + 1]
+      arr[i], arr[i + 1] = arr[i + 1], arr[i]
+      i = 0
+    else
+      i += 1
+    end
+  end
+  arr
 end
-arr
-end
 
-print bubble_sort([4,3,78,2,0,2])
-print bubble_sort([1,2,3,4,5,6])
-
-
+print bubble_sort([4, 3, 78, 2, 0, 2])
+print bubble_sort([1, 2, 3, 4, 5, 6])
